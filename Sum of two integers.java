00b0caab1691d0512ef9db3c45431403c557d5f9
Anup Patil <96967912+itsanup1@users.noreply.github.com>
2026-02-17 22:47:48 +0530

    Add files via upload

---
+++ Sum of two integers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter First Number: ");
+		int num1 = scanner.nextInt();
+		
+		System.out.print("Enter Second Number: ");
+		int num2 = scanner.nextInt();
+		
+		int sum = num1 + num2;
+		
+		System.out.println("The Sum is: "+sum);
+		
+		scanner.close();
+	}
+}
